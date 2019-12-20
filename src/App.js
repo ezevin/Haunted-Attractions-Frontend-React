4aa1d082d3d942949c5748383e206cb27132bd30
ezevin <ehzevin@gmail.com>
2019-12-20 14:58:04 -0500

    fetched attraction data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    attractions: {}
+  }
+
+  componentDidMount(){
+    this.getAllAttractions()
+  }
+
+  getAllAttractions = () => {
+    fetch(`http://localhost:3000/api/v1/attractions`)
+    .then(res => res.json())
+    .then(data => this.setState({attractions: data}))
+  }
+
+  render () {
+    console.log(this.state.attractions);
+    return (
+      <div>
+      HAUNTED ATTRACTIONS
+      </div>
+    );
+
+  }
 }
 
 export default App;
