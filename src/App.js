e54660ec959a59e9f9b3a9ff3fe699267552cb39
ezevin <ehzevin@gmail.com>
2019-12-23 10:50:12 -0500

    retrieved backend data

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 class App extends Component{
   state = {
-    attractions: {}
+    attractions: {},
+    trips: {},
+    users: {}
   }
 
   componentDidMount(){
-    this.getAllAttractions()
+    this.getAllAttractions();
+    this.getAllTrips();
+    this.getAllUsers();
   }
 
   getAllAttractions = () => {
@@ -16,8 +20,22 @@ class App extends Component{
     .then(data => this.setState({attractions: data}))
   }
 
+  getAllTrips = () => {
+    fetch(`http://localhost:3000/api/v1/trips`)
+    .then(res => res.json())
+    .then(data => this.setState({trips: data}))
+  }
+
+  getAllUsers = () => {
+    fetch(`http://localhost:3000/api/v1/users`)
+    .then(res => res.json())
+    .then(data => this.setState({users: data}))
+  }
+
   render () {
     console.log(this.state.attractions);
+    console.log(this.state.trips);
+    console.log(this.state.users);
     return (
       <div>
       HAUNTED ATTRACTIONS
